cd25f156333e4335ce2054c18b27566e617018d0
anderose <94991101+anderose@users.noreply.github.com>
2021-12-15 18:34:07 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  angleMode(DEGREES)
+}
+
+function draw() {
+  background(30);
+  
+  rotateX(0)
+  
+  noFill()
+  stroke(255)
+  
+  for (var i = 0; i < 120; i++)  {
+    
+    var r = map(sin(frameCount / 2), -1, 1, 100, 255)
+    var g = map(i,0, 50, 100, 255)
+    var b = map(cos(frameCount), -1, 1, 200, 100)
+    
+    stroke(r,g,b)
+
+    rotate(frameCount / 100)
+    
+    
+    beginShape()
+    for (var j = 0; j < 360; j += 60) {
+      var rad = i * 3
+      var x = rad * cos(j)
+      var y = rad * sin(j) 
+      var z = sin(frameCount * 2 + i * 5) * 50
+      
+      vertex(x,y,z)
+    } 
+    endShape(CLOSE)
+      
+    
+  }
+  
+  
+}
